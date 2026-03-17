345a13955674de88cf1a7269c09f148c38623929
neostormai <a8069332@gmail.com>
2026-03-17 10:31:22 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import VideoSection from "@/components/VideoSection";
 import Portfolio from "@/components/Portfolio";
 import Philosophy from "@/components/Philosophy";
 import Footer from "@/components/Footer";
@@ -10,7 +9,6 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Header />
       <Hero />
-      <VideoSection />
       <Portfolio />
       <Philosophy />
       <Footer />
